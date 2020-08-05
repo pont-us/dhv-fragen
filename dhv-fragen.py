4791476d3ef03192132d2e508b8a53e21de9c790
Pontus Lurcock <pont@talvi.net>
2020-08-05 07:51:25 +0200

    Process images PDF
    The images PDF is now processed appropriately: the relevant images are
    extracted to the specified directory and named according to their
    accompanying text.

--- dhv-fragen.py
+++ dhv-fragen.py
@@ -14,19 +14,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('questions_pdf')
     parser.add_argument('pictures_pdf')
+    parser.add_argument('pictures_output_dir')
     args = parser.parse_args()
     with tempfile.TemporaryDirectory() as temp_dir:
-        process_files(args.questions_pdf, args.pictures_pdf, temp_dir)
+        process_files(args.questions_pdf, args.pictures_pdf,
+                      args.pictures_output_dir, temp_dir)
 
 
-def process_files(questions_compressed, pictures_pdf, temp_dir):
+def process_files(questions_compressed, pictures_pdf, pictures_dir, temp_dir):
     questions_file = os.path.join(temp_dir, 'questions.pdf')
     decompress_questions_pdf(questions_compressed, questions_file)
     correct_answers = construct_correct_answer_list(questions_file)
     question_list = parse_text_from_pdf(questions_compressed, temp_dir)
     output_matrix = create_output_matrix(question_list, correct_answers)
-    for line in output_matrix:
-        print(line)
+    image_numbers = extract_image_numbers(pictures_pdf, temp_dir)
+    extract_images(pictures_pdf, pictures_dir, image_numbers)
+
+    # for line in output_matrix:
+    #     print(line)
+    # print(image_numbers)
+    # print(len(image_numbers))
 
 
 def decompress_questions_pdf(source, destination):
@@ -74,9 +81,6 @@ def parse_text_from_pdf(questions_pdf: str, temp_dir: str) -> \
         answer_c = 4
         answer_d = 5
 
-    # TODO: handle question numbers properly
-    # Parse the question number and increment the section if the question
-    # number has reset to 1.
     current_section = 1
     last_question_number = 0
     state = State.ignore
@@ -159,13 +163,56 @@ def create_output_matrix(question_list, correct_answers):
         answers = [ans_a, ans_b, ans_c, ans_d]
         correct_answer = answers.pop(correct_index)
         if img_number != 0:
-            # TODO: add the correct image suffix
-            # We'll need the directory of extracted images to do this.
-            img_html = '<img src="%s" alt="Abbildung %d"><br>' % \
+            # We assume that all images are (or have been converted to) PNGs
+            img_html = '<img src="%s.png" alt="Abbildung %d"><br>' % \
                        (make_image_name(img_number), img_number)
             question = img_html + question
         result.append([question, correct_answer] + answers)
     return result
+
+
+def extract_image_numbers(images_filename, temp_dir):
+    text_filename = os.path.join(temp_dir, 'images.txt')
+    subprocess.run(['pdftotext', images_filename, text_filename],
+                   check=True)
+    with open(text_filename, 'r') as fh:
+        lines = fh.readlines()
+    result = []
+    for line in lines:
+        match = re.match(r'^Abbildung (\d+)', line.strip())
+        if match:
+            result.append(int(match.group(1)))
+    return result
+
+
+def extract_images(images_pdf, images_dir, image_numbers):
+    # Empty the directory of any existing files
+    for root, dirs, files in os.walk(images_dir):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+    # We take the lazy option here by asking pdfimages to convert everything
+    # to PNGs. This increases the total size of the images from ~24 MB to
+    # ~77 MB, but saves (1) fiddling about with image extensions, which would
+    # then need to be synced with the img tags in the CSV output, and (2)
+    # explicitly converting the TIFFs, which (apparently) aren't supported in
+    # AnkiDroid.
+    subprocess.run(['pdfimages', '-png', images_pdf,
+                    os.path.join(images_dir, 'x')])
+
+    image_index = 0
+    for filename in sorted(os.listdir(images_dir)):
+        path = os.path.join(images_dir, filename)
+        stat = os.stat(path)
+        if (filename in ['x-089.png', 'x-105.png'] or
+                stat.st_size in [78247, 25220]):
+            os.unlink(path)
+        else:
+            dest = os.path.join(
+                images_dir,
+                make_image_name(image_numbers[image_index]) + '.png'
+            )
+            os.rename(path, dest)
+            image_index += 1
 
 
 def make_image_name(image_number):
